6100ed75b9ff43d963ca5e69426f19bd9c8244b6
SireeshaKare <kvpssireesha@gmail.com>
2025-01-26 21:24:36 +1100

    read data from UI

--- nuix-test/src/redux/itemSlice.ts
+++ nuix-test/src/redux/itemSlice.ts
@@ -1,26 +1,43 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { fetchItems } from "../services/item-api";
 
 export const getItems = createAsyncThunk("items/getItems", fetchItems);
 
-const initialState = {
+export interface Item {
+    guid: string;
+    name: string;
+    path: string[];
+    properties: Record<string, string>;
+}
+export interface Items{
+    items: Item[];
+    selectedItem: Item;
+    error: string;
+    isLoading: boolean;
+}
+
+const initialState: Items = {
     items: [],
-    selectedItem: {},
+    selectedItem: {
+        guid: "",
+        name: "",
+        path: [],
+        properties: {},
+    },
     error: '',
     isLoading: false,
-    
 };
 
 const itemSlice = createSlice({
     name: "items",
     initialState,
     reducers: {
-        selectItem: (state, action) => {
+        selectItem: (state, action: PayloadAction<Item>) => {
             state.selectedItem = action.payload;
         },
     },
     extraReducers: (builder) => {
-        builder.addCase(getItems.fulfilled, (state, action) => {
+        builder.addCase(getItems.fulfilled, (state, action: PayloadAction<Item[]>) => {
             state.items = action.payload;
         })
         .addCase(getItems.rejected, (state, action) => {
